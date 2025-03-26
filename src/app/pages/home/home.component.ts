a262d87b1df93996610f18fd2c55f07a3594429f
FlorezZ1025 <sanaraflorez@gmail.com>
2025-03-26 08:42:32 -0500

    bien

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { SideBarComponent } from "../../components/side-bar/side-bar.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [SideBarComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })

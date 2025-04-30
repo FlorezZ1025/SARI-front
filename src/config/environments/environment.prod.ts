b69e7872a6906087460d833b28ebeccd2daa6ac5
FlorezZ1025 <sanaraflorez@gmail.com>
2025-04-29 19:07:15 -0500

    url prod fixed

--- src/config/environments/environment.prod.ts
+++ src/config/environments/environment.prod.ts
@@ -1,3 +1,3 @@
 export const environment = {
-    API_URL: 'https://cf4c-2803-1800-401e-6ae5-90ca-147a-22ca-9bfa.ngrok-free.app ',
+    API_URL: 'https://cf4c-2803-1800-401e-6ae5-90ca-147a-22ca-9bfa.ngrok-free.app',
 }

2f1646f23cb1d0babee49696e71b50fca47ee989
FlorezZ1025 <sanaraflorez@gmail.com>
2025-05-01 15:12:22 -0500

    new fixed api url

--- src/config/environments/environment.prod.ts
+++ src/config/environments/environment.prod.ts
@@ -1,3 +1,3 @@
 export const environment = {
-    API_URL: 'https://3874-2800-e2-797f-fd5d-d1e4-578-23f1-a41e.ngrok-free.app',
+    API_URL: 'https://3874-2800-e2-797f-fd5d-d1e4-578-23f1-a41e.ngrok-free.app/api',
 }

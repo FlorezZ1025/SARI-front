28bf9bc2d276cbf945a7dbf3212add26bed72322
FlorezZ1025 <sanaraflorez@gmail.com>
2025-05-12 14:52:15 -0500

    sprint 3

--- src/config/environments/environment.prod.ts
+++ src/config/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   API_URL:
-    'https://3874-2800-e2-797f-fd5d-d1e4-578-23f1-a41e.ngrok-free.app/api',
+    'https://1df2-2800-e2-797f-fd5d-d8f7-4311-79da-ad23.ngrok-free.app/api',
 };

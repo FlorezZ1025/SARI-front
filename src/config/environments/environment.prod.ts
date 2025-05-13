104811d254ea892564bc261b9406682ce17afd27
FlorezZ1025 <sanaraflorez@gmail.com>
2025-05-13 10:29:00 -0500

    url

--- src/config/environments/environment.prod.ts
+++ src/config/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   API_URL:
-    'https://1df2-2800-e2-797f-fd5d-d8f7-4311-79da-ad23.ngrok-free.app/api',
+    'https://1cda-2803-1800-400e-8561-c924-c15d-314-e4d3.ngrok-free.app/api',
 };
